899578741a1d0973de3964ac04f9677c67292c34
username <email@email.com>
2016-03-04 12:58:00 +0200

    init

---
+++ SRM656RandomPancakeStackDiv2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRM656RandomPancakeStackDiv2
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public double expectedDeliciousness(int[] d)
+    {
+      return 0.0;
+    }
+  }
+}
