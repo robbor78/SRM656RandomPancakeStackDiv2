4e87dfc419679a1d8e9fa8b9392229e9405b7fb3
username <email@email.com>
2016-03-14 10:24:07 +0200

    random pancakes

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRM656RandomPancakeStackDiv2;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      int[][] allDeliciousnesses = new int[][] {
+        new int[] { 1,1,1}
+        };
+      double[] allExpected = new double[] { 1.6666666666666667d };
+
+      int length = allDeliciousnesses.Length;
+      for (int i = 0; i < length; i++)
+      {
+        int[] deliciousness = allDeliciousnesses[i];
+
+        Program p = new Program();
+
+        double actual = p.expectedDeliciousness(deliciousness);
+
+        double expected = allExpected[i];
+
+        Assert.AreEqual(expected, actual, 1e-6);
+      }
+    }
+  }
+}
